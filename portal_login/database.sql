b2792710020265df56ff8e1342cebc032e5aae11
Luis Carmona <l.carmonaq@gmail.com>
2026-04-30 08:59:42 -0400

    Delete portal_login/database.sql

--- portal_login/database.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE usuarios (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(100),
-  email VARCHAR(150) UNIQUE,
-  password_hash VARCHAR(255),
-  rol ENUM('admin','vendedor','cliente'),
-  estado BOOLEAN DEFAULT 1
-);
